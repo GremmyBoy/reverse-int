18272867aca172ab3c216415616860fe96f4c148
GremmyBoy <bulakea@mail.ru>
2022-07-13 02:46:05 +0300

    feat: add function

--- src/index.js
+++ src/index.js
@@ -1,10 +1,8 @@
 module.exports = function reverse (n) {
-    let y = 0;
-    for(; n ; n = Math.floor(n / 10)){
-       y = y * 10 + n % 10;
-    }
-    return(y);
-}
+        n = n + "";
+        return n.split("").reverse().join("");
+      }
+
 
   
 
